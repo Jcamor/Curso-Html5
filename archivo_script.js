70c2cfa8d3f8fcd5d0d952cb4aced976f16aa4c6
Juan Carlos Amor <49997664+Jcamor@users.noreply.github.com>
2019-06-19 18:32:41 +0200

    video13

--- archivo_script.js
+++ archivo_script.js
@@ -1,5 +1,18 @@
 /* Primer javascript juancarlosamor.es*/
 
+function ejecuta(){
+   /* for(var i=0; i<3; i++){
+     document.getElementsByTagName("p")[i].onclick=saludo;
+    }
+ // document.getElementById("importante").onclick=saludo;*/
+   
+    for(var i=0; i<2; i++){
+        var z=document.getElementsByClassName("importante")[i].onclick=saludo;
+    }
+}
+
 function saludo(){
     alert("Que hay de nuevo");
 }
+
+window.onload=ejecuta;
