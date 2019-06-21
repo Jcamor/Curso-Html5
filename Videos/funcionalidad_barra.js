404e8a0add0dfc85849a2b7e77b1bc82e94071c7
Juan Carlos Amor <49997664+Jcamor@users.noreply.github.com>
2019-06-22 00:13:49 +0200

    video19

--- Videos/funcionalidad_barra.js
+++ Videos/funcionalidad_barra.js
@@ -1,5 +1,5 @@
-var mivideo, reproducir, barra, progreso;
-
+var mivideo, reproducir, barra, progreso,maximo;
+maximo=600;
 function comenzar(){
     mivideo=document.getElementById("mivideo");
     reproducir=document.getElementById("reproducir");
@@ -7,7 +7,7 @@ function comenzar(){
     progreso=document.getElementById("progreso");
 
     reproducir.addEventListener("click",clicando,false);
-    progreso.addEventListener("click",adelantando,false);
+    barra.addEventListener("click",adelantando,false);
 }
 
 function clicando(){
@@ -17,6 +17,25 @@ function clicando(){
     }else{
         mivideo.play();
         reproducir.innerHTML="Pause";
+        bucle=setInterval(estado,100);
+    }
+}
+
+function estado(){
+    if(mivideo.ended==false){
+        var total = parseInt(mivideo.currentTime*maximo/mivideo.duration);
+        progreso.style.width=total+"px";
+    }
+
+}
+
+function adelantando(posicion){
+    if((mivideo.paused==false) && (mivideo.ended)==false){
+        var ratonX=posicion.pageX-barra.offsetLeft;
+        var nuevoTiempo=ratonX*mivideo.duration/maximo;
+        
+        mivideo.currentTime=nuevoTiempo;
+        progreso.style.width=ratonX+"px";
     }
 }
 
