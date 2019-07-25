6dcc18094fbf97eae38d9f288d5a2a85b92b0696
Juan Carlos Amor <49997664+Jcamor@users.noreply.github.com>
2019-07-25 20:36:30 +0200

    Video 52

--- indexed.js
+++ indexed.js
@@ -3,17 +3,33 @@ var bd;
 function iniciar() {
     zonadatos = document.getElementById("zonadatos");
     boton = document.getElementById("grabar");
-    boton.addEventListener("click", agregoobjeto, false);
+    boton.addEventListener("click", agregarobjeto, false);
 
-    var solicitud = indexedDB.open("mibase");
+    var solicitud = indexedDB.open("mibase2");
 
     solicitud.onsuccess = function(e) {
         bd = e.target.result;
     }
 
     solicitud.onupgradeneeded = function(e) {
+        bd = e.target.result;
         bd.createObjectStore("gente", { keyPath: "clave" });
     }
+}
+
+function agregarobjeto() {
+    var clave = document.getElementById("clave").value;
+    var titulo = document.getElementById("texto").value;
+    var fecha = document.getElementById("fecha").value;
+
+    var transaccion = bd.transaction(["gente"], "readwrite");
+    var almacen = transaccion.objectStore("gente");
+    var agregar = almacen.add({ clave: clave, titulo: titulo, fecha: fecha });
+
+    document.getElementById("clave").value = "";
+    document.getElementById("texto").value = "";
+    document.getElementById("fecha").value = "";
+
 }
 
 
