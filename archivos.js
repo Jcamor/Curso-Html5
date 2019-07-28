aee94a28c69974e7dff12d1d48a8ff41b216b541
Juan Carlos Amor <49997664+Jcamor@users.noreply.github.com>
2019-07-28 15:44:19 +0200

    Video 55

--- archivos.js
+++ archivos.js
@@ -7,17 +7,30 @@ function comenzar() {
 
 function procesar(e) {
     var archivos = e.target.files;
-    var miarchivo = archivos[0];
-    var lector = new FileReader();
 
-    lector.readAsText(miarchivo, "iso-8859-1");
-    lector.addEventListener("load", mostar_en_web, false);
+    zonadatos.innerHTML = "";
+
+    var miarchivo = archivos[0]; // el primer archivo seleccionado
+
+    //alert(miarchivo.type); para sacar el tipo de archivo
+    if (!miarchivo.type.match(/image/)) {
+
+        alert("Selecciona una imagen, for favor");
+    } else {
+        zonadatos.innerHTML += "Nombre del archivo: " + miarchivo.name + "<br>";
+        zonadatos.innerHTML += "Tamaño del archivo: " + Math.round(miarchivo.size / 1024) + "Kb <br>";
+        var lector = new FileReader();
+        lector.readAsDataURL(miarchivo);
+        lector.addEventListener("load", mostar_en_web, false);
+    }
+    //lector.readAsText(miarchivo, "iso-8859-1"); // se lee el archivo en formato europa occidental
+
 }
 
 function mostar_en_web(e) {
     var resultado = e.target.result;
 
-    zonadatos.innerHTML = resultado;
+    zonadatos.innerHTML += "<img src='" + resultado + "' width='95%'>";
 }
 
 
